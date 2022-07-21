3498280c28f23248b29ce6ca9ad03294aff08829
Ibrahima Ciss <bionik6@gmail.com>
2022-07-21 09:34:15 +0000

    Initialize SPM project

--- Package.swift
+++ Package.swift
@@ -1,28 +1,19 @@
 // swift-tools-version: 5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
-    name: "greenstand-treetracker-core",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "greenstand-treetracker-core",
-            targets: ["greenstand-treetracker-core"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "greenstand-treetracker-core",
-            dependencies: []),
-        .testTarget(
-            name: "greenstand-treetracker-coreTests",
-            dependencies: ["greenstand-treetracker-core"]),
-    ]
+  name: "greenstand-treetracker-core",
+  platforms: [.iOS(.v11)],
+  products: [
+    .library(
+      name: "greenstand-treetracker-core",
+      targets: ["greenstand-treetracker-core"]
+    ),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(name: "greenstand-treetracker-core", dependencies: []),
+    .testTarget(name: "greenstand-treetracker-coreTests", dependencies: ["greenstand-treetracker-core"]),
+  ]
 )
